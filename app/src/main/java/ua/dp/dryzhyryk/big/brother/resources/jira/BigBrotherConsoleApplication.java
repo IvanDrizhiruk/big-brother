a2622df9a2b88324f73d03ba4c1526c4064558ee
Ivan Dryzhyruk <IDryzhyruk@luxoft.com>
2019-11-19 18:25:57 +0200

    Restore runnability of the app

--- app/src/main/java/ua/dp/dryzhyryk/big/brother/resources/jira/BigBrotherConsoleApplication.java
+++ app/src/main/java/ua/dp/dryzhyryk/big/brother/resources/jira/BigBrotherConsoleApplication.java
@@ -1,7 +1,20 @@
 package ua.dp.dryzhyryk.big.brother.resources.jira;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.stream.Stream;
+
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import lombok.extern.slf4j.Slf4j;
 import ua.dp.dryzhyryk.big.brother.core.BigJiraBrother;
 import ua.dp.dryzhyryk.big.brother.core.data.source.JiraInformationCache;
 import ua.dp.dryzhyryk.big.brother.core.data.source.JiraInformationHolder;
@@ -10,74 +23,114 @@ import ua.dp.dryzhyryk.big.brother.core.data.source.model.search.SprintSearchCon
 import ua.dp.dryzhyryk.big.brother.core.ports.JiraResource;
 import ua.dp.dryzhyryk.big.brother.data.extractor.jira.JiraDataExtractor;
 
-import java.net.URI;
-
+@Slf4j
 public class BigBrotherConsoleApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        URI uri = URI.create("https://jira.dp.ua");
-        String username = "mega_user";
-        String password = "mega_password";
+		Optional<String> configDir = extract("configDir", args);
 
-        AsynchronousJiraRestClientFactory jiraRestClientFactory = new AsynchronousJiraRestClientFactory();
-        JiraRestClient jiraRestClient = jiraRestClientFactory.createWithBasicHttpAuthentication(uri, username, password);
+		String jiraFilePath = configDir
+				.map(path -> path + "/jira.properties")
+				.orElse("jira.properties");
 
+		Properties jiraProperties = loadProperties(jiraFilePath);
 
-        JiraResource jiraResource = new JiraDataExtractor(jiraRestClient);
-        JiraInformationCache jiraInformationCache = new JiraInformationCache(jiraResource);
-        JiraInformationHolder jiraInformationHolder = new JiraInformationHolder(jiraInformationCache);
-        BigJiraBrother bigJiraBrother = new BigJiraBrother(jiraInformationHolder);
+		URI uri = URI.create(jiraProperties.getProperty("jira.url"));
+		String username = jiraProperties.getProperty("jira.user");
+		String password = jiraProperties.getProperty("jira.password");
 
+		AsynchronousJiraRestClientFactory jiraRestClientFactory = new AsynchronousJiraRestClientFactory();
+		JiraRestClient jiraRestClient = jiraRestClientFactory.createWithBasicHttpAuthentication(uri, username, password);
 
-        SprintSearchConditions sprintSearchConditions = SprintSearchConditions
-                .builder()
-                .project("PROJECT_NAME")
-                .sprint("SPRINT_NAME")
-                .build();
+		JiraResource jiraResource = new JiraDataExtractor(jiraRestClient);
+		JiraInformationCache jiraInformationCache = new JiraInformationCache(jiraResource);
+		JiraInformationHolder jiraInformationHolder = new JiraInformationHolder(jiraInformationCache);
+		BigJiraBrother bigJiraBrother = new BigJiraBrother(jiraInformationHolder);
 
+		String serchFilePath = configDir
+				.map(path -> path + "/search.json")
+				.orElse("search.json");
 
-        TasksTree tasksTree = jiraInformationHolder.getTasksAsTree(sprintSearchConditions);
+		SprintSearchConditions sprintSearchConditions = loadJson(serchFilePath, SprintSearchConditions.class);
 
-        //		Gson gson = (new GsonBuilder()).create();
-        //
-        //		System.out.println(gson.toJson(tasksTree));
+		TasksTree tasksTree = jiraInformationHolder.getTasksAsTree(sprintSearchConditions);
 
-        tasksTree.getRootTasks().forEach(task -> {
-            System.out.println(task.getId() + " " + task.getName());
-            System.out.println(
-                    "Estimated " + convertMinutesToHour(task.getOriginalEstimateMinutes()) +
-                            " Real " + convertMinutesToHour(task.getTimeSpentMinutes()) +
-                            " Remaining " + convertMinutesToHour(task.getRemainingEstimateMinutes()));
+		//		Gson gson = (new GsonBuilder()).create();
+		//
+		//		System.out.println(gson.toJson(tasksTree));
 
-            task.getWorkLogs().forEach(worklog -> {
-                System.out.println(worklog.getPerson() + " " + worklog.getMinutesSpent() + " " + worklog.getStartDateTime());
-            });
+		tasksTree.getRootTasks().forEach(task -> {
+			System.out.println(task.getId() + " " + task.getName());
+			System.out.println(
+					"Estimated " + convertMinutesToHour(task.getOriginalEstimateMinutes()) +
+							" Real " + convertMinutesToHour(task.getTimeSpentMinutes()) +
+							" Remaining " + convertMinutesToHour(task.getRemainingEstimateMinutes()));
 
-            task.getSubTasks().forEach(subTask -> {
-                System.out.println("          " + subTask.getId() + " " + subTask.getName());
-                System.out.println("          " +
-                        "Estimated " + subTask.getOriginalEstimateMinutes() +
-                        " Real " + subTask.getTimeSpentMinutes() +
-                        " Remaining " + subTask.getRemainingEstimateMinutes());
+			task.getWorkLogs().forEach(worklog -> {
+				System.out.println(worklog.getPerson() + " " + worklog.getMinutesSpent() + " " + worklog.getStartDateTime());
+			});
 
-                subTask.getWorkLogs().forEach(worklog -> {
-                    System.out.println("          " + worklog.getPerson() + " "
-                            + convertMinutesToHour(worklog.getMinutesSpent()) + " "
-                            + worklog.getStartDateTime());
-                });
+			task.getSubTasks().forEach(subTask -> {
+				System.out.println("          " + subTask.getId() + " " + subTask.getName());
+				System.out.println("          " +
+						"Estimated " + subTask.getOriginalEstimateMinutes() +
+						" Real " + subTask.getTimeSpentMinutes() +
+						" Remaining " + subTask.getRemainingEstimateMinutes());
 
-                System.out.println("");
-            });
-            System.out.println("");
+				subTask.getWorkLogs().forEach(worklog -> {
+					System.out.println("          " + worklog.getPerson() + " "
+							+ convertMinutesToHour(worklog.getMinutesSpent()) + " "
+							+ worklog.getStartDateTime());
+				});
 
-        });
-    }
+				System.out.println("");
+			});
+			System.out.println("");
 
-    private static Integer convertMinutesToHour(Integer minutes) {
-        if (null == minutes || minutes.equals(0)) {
-            return 0;
-        }
-        return minutes / 60;
-    }
+		});
+	}
+
+	private static <T> T loadJson(String serchFilePath, Class<T> clazz) {
+		Gson gson = (new GsonBuilder()).create();
+		try (FileReader fileReader = new FileReader(serchFilePath)) {
+			return gson.fromJson(fileReader, clazz);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Optional<String> extract(String parameterName, String[] args) {
+		return Stream.of(args)
+				.filter(arg -> arg.startsWith(parameterName))
+				.map(arg -> arg.substring(parameterName.length() + 1))
+				.findFirst();
+	}
+
+	private static Properties loadProperties(String fileName) {
+		try (InputStream input = new FileInputStream("/home/idryzhyruk/workspace/luxoft/big-brother/config/jira.properties")) {
+
+			if (input == null) {
+				log.error("Sorry, unable to find {}", fileName);
+				throw new IllegalArgumentException("Sorry, unable to find " + fileName);
+			}
+
+			Properties prop = new Properties();
+			prop.load(input);
+
+			return prop;
+
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Integer convertMinutesToHour(Integer minutes) {
+		if (null == minutes || minutes.equals(0)) {
+			return 0;
+		}
+		return minutes / 60;
+	}
 }
--- core/src/main/java/ua/dp/dryzhyryk/big/brother/core/BigJiraBrother.java
+++ core/src/main/java/ua/dp/dryzhyryk/big/brother/core/BigJiraBrother.java
@@ -1,40 +1,42 @@
 package ua.dp.dryzhyryk.big.brother.core;
 
-import ua.dp.dryzhyryk.big.brother.core.data.source.JiraInformationHolder;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
 import ua.dp.dryzhyryk.big.brother.core.aggregators.model.PeopleView;
+import ua.dp.dryzhyryk.big.brother.core.data.source.JiraInformationHolder;
 import ua.dp.dryzhyryk.big.brother.core.data.source.model.TasksTree;
 import ua.dp.dryzhyryk.big.brother.core.data.source.model.TasksTreeView;
 import ua.dp.dryzhyryk.big.brother.core.data.source.model.search.SprintSearchConditions;
 
-import java.util.List;
-
+@Slf4j
 public class BigJiraBrother {
 
-    private final JiraInformationHolder jiraInformationHolder;
+	private final JiraInformationHolder jiraInformationHolder;
 
-    public BigJiraBrother(JiraInformationHolder jiraInformationHolder) {
-        this.jiraInformationHolder = jiraInformationHolder;
-    }
+	public BigJiraBrother(JiraInformationHolder jiraInformationHolder) {
+		this.jiraInformationHolder = jiraInformationHolder;
+	}
 
-    public List<TasksTreeView> prepareTaskView(SprintSearchConditions sprintSearchConditions) {
-        TasksTree tasksTree = jiraInformationHolder.getTasksAsTree(sprintSearchConditions);
+	public List<TasksTreeView> prepareTaskView(SprintSearchConditions sprintSearchConditions) {
+		TasksTree tasksTree = jiraInformationHolder.getTasksAsTree(sprintSearchConditions);
 
-        //		List<TasksTree> tasksTrees = projectsKeys.stream()
-        //				.map(projectKey -> jiraInformationHolder.getTasksAsTree(projectKey, startDate, endDate))
-        //				.collect(Collectors.toList());
+		//		List<TasksTree> tasksTrees = projectsKeys.stream()
+		//				.map(projectKey -> jiraInformationHolder.getTasksAsTree(projectKey, startDate, endDate))
+		//				.collect(Collectors.toList());
 
-        //		TasksTreeViewAggregator tasksTreeViewAggregator = new TasksTreeViewAggregator();
+		//		TasksTreeViewAggregator tasksTreeViewAggregator = new TasksTreeViewAggregator();
 
-        //		return tasksTrees.stream()
-        //				.map(tasksTreeViewAggregator::prepareTasksTreeView)
-        //				.collect(Collectors.toList());
-        return null;
-    }
+		//		return tasksTrees.stream()
+		//				.map(tasksTreeViewAggregator::prepareTasksTreeView)
+		//				.collect(Collectors.toList());
+		return null;
+	}
 
-    public PeopleView preparePeopleView() {
-        return new PeopleView();
-    }
+	public PeopleView preparePeopleView() {
+		return new PeopleView();
+	}
 
-    public void prepareDayView() {
-    }
+	public void prepareDayView() {
+	}
 }
