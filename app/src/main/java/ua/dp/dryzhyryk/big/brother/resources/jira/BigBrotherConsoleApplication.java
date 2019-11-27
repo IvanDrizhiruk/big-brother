278a50a6242225314ed9c17a444918d11db23059
Ivan Dryzhyruk <IDryzhyruk@luxoft.com>
2019-11-27 18:30:07 +0200

    Clean code in app

--- app/src/main/java/ua/dp/dryzhyryk/big/brother/resources/jira/BigBrotherConsoleApplication.java
+++ app/src/main/java/ua/dp/dryzhyryk/big/brother/resources/jira/BigBrotherConsoleApplication.java
@@ -1,131 +1,83 @@
 package ua.dp.dryzhyryk.big.brother.resources.jira;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.util.Arrays;
+
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import lombok.extern.slf4j.Slf4j;
 import ua.dp.dryzhyryk.big.brother.core.BigJiraBrother;
 import ua.dp.dryzhyryk.big.brother.core.BigJiraBrotherPeopleView;
 import ua.dp.dryzhyryk.big.brother.core.data.source.JiraInformationHolder;
 import ua.dp.dryzhyryk.big.brother.core.data.source.model.search.PeopleSearchConditions;
-import ua.dp.dryzhyryk.big.brother.core.data.source.model.search.PersonSearchConditions;
 import ua.dp.dryzhyryk.big.brother.core.data.source.model.search.SprintSearchConditions;
 import ua.dp.dryzhyryk.big.brother.core.metrics.calculator.PeopleViewMetricsCalculator;
 import ua.dp.dryzhyryk.big.brother.core.metrics.calculator.SprintViewMetricsCalculator;
 import ua.dp.dryzhyryk.big.brother.core.metrics.calculator.TasksTreeViewMetricsCalculator;
 import ua.dp.dryzhyryk.big.brother.core.metrics.calculator.model.PeopleView;
+import ua.dp.dryzhyryk.big.brother.core.metrics.calculator.model.SprintView;
+import ua.dp.dryzhyryk.big.brother.core.metrics.calculator.model.TasksTreeView;
 import ua.dp.dryzhyryk.big.brother.core.ports.JiraDataStorage;
 import ua.dp.dryzhyryk.big.brother.core.ports.JiraResource;
 import ua.dp.dryzhyryk.big.brother.data.extractor.jira.JiraDataExtractor;
 import ua.dp.dryzhyryk.big.brother.data.storage.jira.JiraFileDataStorage;
 import ua.dp.dryzhyryk.big.brother.report.generator.excel.ExcelReportGenerator;
 
-import java.io.*;
-import java.net.URI;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Properties;
-import java.util.stream.Stream;
-
 @Slf4j
 public class BigBrotherConsoleApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Optional<String> rootDir = extract("rootDir", args);
+		Configurations config = Configurations.loadFromAppArguments(args);
 
-        String jiraFilePath = rootDir
-                .map(path -> path + "config/jira.properties")
-                .orElse("config/jira.properties");
+		File storageRoot = new File(config.getRootDir(), "storage");
+		storageRoot.mkdirs();
 
-        Properties jiraProperties = loadProperties(jiraFilePath);
+		File reportRoot = new File(config.getRootDir(), "/reports");
+		reportRoot.mkdirs();
 
-        URI uri = URI.create(jiraProperties.getProperty("jira.url"));
-        String username = jiraProperties.getProperty("jira.user");
-        String password = jiraProperties.getProperty("jira.password");
+		String searchFilePath = config.getRootDir() + "/search.json";
 
-        AsynchronousJiraRestClientFactory jiraRestClientFactory = new AsynchronousJiraRestClientFactory();
-        JiraRestClient jiraRestClient = jiraRestClientFactory.createWithBasicHttpAuthentication(uri, username, password);
+		AsynchronousJiraRestClientFactory jiraRestClientFactory = new AsynchronousJiraRestClientFactory();
+		JiraRestClient jiraRestClient = jiraRestClientFactory
+				.createWithBasicHttpAuthentication(config.getJiraUri(), config.getJiraUsername(), config.getJiraPassword());
+		JiraResource jiraResource = new JiraDataExtractor(jiraRestClient);
+		JiraDataStorage jiraDataStorage = new JiraFileDataStorage(storageRoot.getAbsolutePath());
+		JiraInformationHolder jiraInformationHolder = new JiraInformationHolder(jiraResource, jiraDataStorage);
+		TasksTreeViewMetricsCalculator tasksTreeViewMetricsCalculator = new TasksTreeViewMetricsCalculator();
+		PeopleViewMetricsCalculator peopleViewMetricsCalculator = new PeopleViewMetricsCalculator();
+		SprintViewMetricsCalculator sprintViewMetricsCalculator = new SprintViewMetricsCalculator();
+		BigJiraBrother bigJiraBrother = new BigJiraBrother(jiraInformationHolder, tasksTreeViewMetricsCalculator, peopleViewMetricsCalculator,
+				sprintViewMetricsCalculator);
 
-        File storageRoot = new File(rootDir.orElse("./"), "storage");
-        storageRoot.mkdirs();
+		BigJiraBrotherPeopleView bigJiraBrotherPeopleView = new BigJiraBrotherPeopleView(jiraInformationHolder, peopleViewMetricsCalculator);
 
-        JiraResource jiraResource = new JiraDataExtractor(jiraRestClient);
-        JiraDataStorage jiraDataStorage = new JiraFileDataStorage(storageRoot.getPath());
-        JiraInformationHolder jiraInformationHolder = new JiraInformationHolder(jiraResource, jiraDataStorage);
-        TasksTreeViewMetricsCalculator tasksTreeViewMetricsCalculator = new TasksTreeViewMetricsCalculator();
-        PeopleViewMetricsCalculator peopleViewMetricsCalculator = new PeopleViewMetricsCalculator();
-        SprintViewMetricsCalculator sprintViewMetricsCalculator = new SprintViewMetricsCalculator();
-        BigJiraBrother bigJiraBrother = new BigJiraBrother(jiraInformationHolder, tasksTreeViewMetricsCalculator, peopleViewMetricsCalculator,
-                sprintViewMetricsCalculator);
+		ExcelReportGenerator reportGenerator = new ExcelReportGenerator(reportRoot.getAbsolutePath());
 
-        BigJiraBrotherPeopleView bigJiraBrotherPeopleView = new BigJiraBrotherPeopleView(jiraInformationHolder, peopleViewMetricsCalculator);
+		SprintSearchConditions sprintSearchConditions = JsonUtils.loadJson(searchFilePath, SprintSearchConditions.class);
+		PeopleSearchConditions peopleSearchConditions = PeopleSearchConditions.builder()
+				.teamName("Ducks")
+				.peopleNames(Arrays.asList("o_dkoval", "o_ssolov", "o_okolom", "o_ystepa", "o_izhytn"))
+				.startPeriod(LocalDate.of(2019, 11, 11))
+				.endPeriod(LocalDate.of(2019, 11, 15))
+				.build();
 
-        String serchFilePath = rootDir
-                .map(path -> path + "/search.json")
-                .orElse("search.json");
+		prepareReportBySprint(sprintSearchConditions, bigJiraBrother, reportGenerator);
+		prepareReportByPerson(peopleSearchConditions, bigJiraBrotherPeopleView, reportGenerator);
+	}
 
-        SprintSearchConditions sprintSearchConditions = loadJson(serchFilePath, SprintSearchConditions.class);
+	private static void prepareReportByPerson(PeopleSearchConditions peopleSearchConditions, BigJiraBrotherPeopleView bigJiraBrotherPeopleView,
+			ExcelReportGenerator reportGenerator) {
+		PeopleView peopleView = bigJiraBrotherPeopleView.preparePeopleView(peopleSearchConditions);
+		reportGenerator.generateReport(peopleView);
+	}
 
-//		TasksTreeView tasksTreeView = bigJiraBrother.prepareTaskView(sprintSearchConditions);
-//		SprintView sprintView = bigJiraBrother.prepareSprintView(sprintSearchConditions);
-
-        File reportRoot = new File(rootDir.orElse("./"), "/reports");
-        reportRoot.mkdirs();
-        ExcelReportGenerator reportGenerator = new ExcelReportGenerator(reportRoot.getAbsolutePath());
-//		reportGenerator.generateReport(tasksTreeView, sprintView);
-
-        PeopleSearchConditions peopleSearchConditions = PeopleSearchConditions.builder()
-                .teamName("Ducks")
-                .peopleNames(Arrays.asList("o_dkoval","o_ssolov", "o_okolom", "o_ystepa", "o_izhytn"))
-                .startPeriod(LocalDate.of(2019, 11, 11))
-                .endPeriod(LocalDate.of(2019, 11, 15))
-                .build();
-
-        PeopleView peopleView = bigJiraBrotherPeopleView.preparePeopleView(peopleSearchConditions);
-        reportGenerator.generateReport(peopleView);
-    }
-
-    private static <T> T loadJson(String serchFilePath, Class<T> clazz) {
-        Gson gson = (new GsonBuilder()).create();
-        try (FileReader fileReader = new FileReader(serchFilePath)) {
-            return gson.fromJson(fileReader, clazz);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Optional<String> extract(String parameterName, String[] args) {
-        return Stream.of(args)
-                .filter(arg -> arg.startsWith(parameterName))
-                .map(arg -> arg.substring(parameterName.length() + 1))
-                .findFirst();
-    }
-
-    private static Properties loadProperties(String fileName) {
-        try (InputStream input = new FileInputStream(fileName)) {
-
-            if (input == null) {
-                log.error("Sorry, unable to find {}", fileName);
-                throw new IllegalArgumentException("Sorry, unable to find " + fileName);
-            }
-
-            Properties prop = new Properties();
-            prop.load(input);
-
-            return prop;
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Integer convertMinutesToHour(Integer minutes) {
-        if (null == minutes || minutes.equals(0)) {
-            return 0;
-        }
-        return minutes / 60;
-    }
+	private static void prepareReportBySprint(SprintSearchConditions sprintSearchConditions, BigJiraBrother bigJiraBrother,
+			ExcelReportGenerator reportGenerator) {
+		TasksTreeView tasksTreeView = bigJiraBrother.prepareTaskView(sprintSearchConditions);
+		SprintView sprintView = bigJiraBrother.prepareSprintView(sprintSearchConditions);
+		reportGenerator.generateReport(tasksTreeView, sprintView);
+	}
 }
---
+++ app/src/main/java/ua/dp/dryzhyryk/big/brother/resources/jira/Configurations.java
@@ -0,0 +1,82 @@
+package ua.dp.dryzhyryk.big.brother.resources.jira;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Configurations {
+
+	private final String rootDir;
+	private final URI jiraUri;
+	private final String jiraUsername;
+	private final String jiraPassword;
+
+	public Configurations(String rootDir, URI jiraUri, String jiraUsername, String jiraPassword) {
+		this.rootDir = rootDir;
+		this.jiraUri = jiraUri;
+		this.jiraUsername = jiraUsername;
+		this.jiraPassword = jiraPassword;
+	}
+
+	public String getRootDir() {
+		return rootDir;
+	}
+
+	public URI getJiraUri() {
+		return jiraUri;
+	}
+
+	public String getJiraUsername() {
+		return jiraUsername;
+	}
+
+	public String getJiraPassword() {
+		return jiraPassword;
+	}
+
+	public static Configurations loadFromAppArguments(String[] args) {
+		String rootDir = extract("rootDir", args).orElse("./");
+
+		Properties jiraProperties = loadProperties(rootDir + "config/jira.properties");
+
+		URI jiraUri = URI.create(jiraProperties.getProperty("jira.url"));
+		String jiraUsername = jiraProperties.getProperty("jira.user");
+		String jiraPassword = jiraProperties.getProperty("jira.password");
+
+		return new Configurations(rootDir, jiraUri, jiraUsername, jiraPassword);
+	}
+
+	private static Optional<String> extract(String parameterName, String[] args) {
+		return Stream.of(args)
+				.filter(arg -> arg.startsWith(parameterName))
+				.map(arg -> arg.substring(parameterName.length() + 1))
+				.findFirst();
+	}
+
+	private static Properties loadProperties(String fileName) {
+		try (InputStream input = new FileInputStream(fileName)) {
+
+			if (input == null) {
+				log.error("Sorry, unable to find {}", fileName);
+				throw new IllegalArgumentException("Sorry, unable to find " + fileName);
+			}
+
+			Properties prop = new Properties();
+			prop.load(input);
+
+			return prop;
+
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+
